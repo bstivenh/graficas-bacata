228f18f8e2c3048ca4bfb844fb410915f9e851b2
Stiven Hernandez <hbrayan1216@gmail.com>
2024-05-02 01:39:56 -0500

    🔥 Ajuste código

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function(){
+    window.sr = ScrollReveal();
+    if ($(window).width() < 768) {
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+
+        sr.reveal('.js--fadeInRight', {
+            origin: 'right',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+    } else {
+        sr.reveal('.js--fadeInLeft', {
+            origin: 'left',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+        sr.reveal('.js--fadeInRight', {
+            origin: 'right',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+        origin: 'left',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+});
+
+window.onload = function() {
+    $('#onload').fadeOut(); 
+    $('body').removeClass('not-show');
+    $('#icode-backgronud').removeClass('hidden');
+    $('#onload').addClass('hidden');
+
+    const copyright = new Date();
+    const update = copyright.getFullYear();
+    let year = document.getElementById('year-footer');
+    year.innerHTML = `&copy; ${update} <a href="#" target="_blank">Gráficas Bacatá</a>`;
+}
--- assets/js/preload.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    $('#onload').fadeOut(); 
-    $('body').removeClass('not-show');
-    $('#icode-backgronud').removeClass('hidden');
-    $('#onload').addClass('hidden');
-}
